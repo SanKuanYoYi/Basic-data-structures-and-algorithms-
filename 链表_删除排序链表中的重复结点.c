e96feedfc28cd5ee11057b6342d84871538388fd
SanKuanYoYi <1575424170@qq.com>
2025-12-28 20:20:57 +0800

    Linked list - Delete duplicate nodes in the sorted linked list
    Given the head of a sorted linked list, head, remove all duplicate elements so that each element appears only once. Return the sorted linked list.

---
+++ 链表_删除排序链表中的重复结点.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+
+    if (head == NULL) {
+        return NULL;
+    }
+
+    struct ListNode* p = head;
+    while (p != NULL && p->next != NULL) {
+        if (p->val == p->next->val) {
+            struct ListNode* del = p->next;
+            p->next = p->next->next;
+            free(del);
+        } else {
+            p = p->next;
+        }
+    }
+
+    return head;
+}
